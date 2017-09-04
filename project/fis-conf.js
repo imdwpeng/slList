b7faae61efc2845a43102b8e9d2081f8066d4b32
dwp <dwp@chengxu-mac.local>
2017-09-04 16:05:58 +0800

    no message

--- project/fis-conf.js
+++ project/fis-conf.js
@@ -41,7 +41,7 @@ if (arguments == 'prod') {
                 to: '../'
             })
         })
-        .match('resource/**/*.{js, css}', {
+        .match('resource/*.{js, css}', {
             domain:'slList',
             url: '$0'
         })
